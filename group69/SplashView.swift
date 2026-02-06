442a69a4cc32131c9790d0db951d6f496926b28d
Sokmontrey <sythatsokmontrey@gmail.com>
2026-02-06 12:15:13 -0500

    feat: implement splash screen

--- group69/SplashView.swift
+++ group69/SplashView.swift
@@ -8,7 +8,34 @@
 import SwiftUI
 
 struct SplashView: View {
+    @State private var scale = 0.8
+    @State private var opacity = 0.5
+
     var body: some View {
+        ZStack {
+            Color.white
+                .ignoresSafeArea()
+
+            VStack(spacing: 16) {
+                Image(systemName: "calendar.badge.plus")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50)
+                    .foregroundColor(.blue)
+
+                Text("Tasky")
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+            }
+            .scaleEffect(scale)
+            .opacity(opacity)
+            .onAppear {
+                withAnimation(.easeInOut(duration: 1.2)) {
+                    scale = 1.0
+                    opacity = 1.0
+                }
+            }
+        }
     }
 }
 
