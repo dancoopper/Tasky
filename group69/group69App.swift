50acf40df61d5466bc9f906efa290c4ecf5f76d6
Sokmontrey <sythatsokmontrey@gmail.com>
2026-03-13 13:12:44 -0400

    feat: setup project for early prototype

--- group69/group69App.swift
+++ group69/group69App.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 @main
 struct group69App: App {
+    @StateObject private var dataStore = DataStore()
+    @StateObject private var settingsStore = SettingsStore.shared
+    @Environment(\.scenePhase) private var scenePhase
+
     var body: some Scene {
         WindowGroup {
             RootView()
+                .environmentObject(dataStore)
+                .environmentObject(settingsStore)
+                .preferredColorScheme(settingsStore.isDarkMode ? .dark : .light)
+                .onChange(of: scenePhase) { phase in
+                    if phase == .inactive {
+                        dataStore.save()
+                    } else if phase == .active {
+                        dataStore.load()
+                    }
+                }
         }
     }
 }
